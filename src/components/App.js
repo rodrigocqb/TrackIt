21e4db75fc327452922413a6dd7b72f22525643f
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-03 00:13:29 -0300

    App component structure done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "../style/globalStyle";
+import Habits from "./Habits";
+import History from "./History";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Today from "./Today";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<SignUp />} />
+        <Route path="/habitos" element={<Habits />} />
+        <Route path="/hoje" element={<Today />} />
+        <Route path="/historico" element={<History />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
