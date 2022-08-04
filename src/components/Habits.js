90b8712d3daaef54eabb4236e05505f85e1f7c2c
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-04 16:17:20 -0300

    New habit form almost done

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -10,6 +10,8 @@ import Header from "./Header";
 
 export default function Habits() {
     const [userHabits, setUserHabits] = useState([]);
+    const [disabled, setDisabled] = useState(false);
+    const [newHabit, setNewHabit] = useState({ habit: "", open: false });
 
     const { token } = useContext(LoginContext);
 
@@ -21,7 +23,21 @@ export default function Habits() {
             .catch(() => {
                 alert("Houve um erro ao carregar os hábitos");
             });
-    }, []);
+    }, [token]);
+
+    function handleNewHabit({ value, name }) {
+        setNewHabit({
+            ...newHabit,
+            [name]: value,
+        });
+    }
+
+    function showHabitForm() {
+        setNewHabit({
+            ...newHabit,
+            open: !newHabit.open,
+        });
+    }
 
     return (
         <>
@@ -29,11 +45,35 @@ export default function Habits() {
             <MainAuth>
                 <TitleSection Button={true}>
                     <h1>Meus hábitos</h1>
-                    <AddButton>+</AddButton>
+                    <AddButton onClick={showHabitForm}>+</AddButton>
                 </TitleSection>
-                <FormSection>
-                    <Input></Input>
-                </FormSection>
+                {newHabit.open && (
+                    <FormSection>
+                        <form>
+                            <Input
+                                type="text"
+                                placeholder="nome do hábito"
+                                name="habit"
+                                value={newHabit.habit}
+                                onChange={(e) => {
+                                    handleNewHabit({
+                                        name: e.target.name,
+                                        value: e.target.value,
+                                    });
+                                }}
+                                disabled={disabled}
+                                required
+                            ></Input>
+                            <DaysContainer>
+                                <div>gerso</div>
+                            </DaysContainer>
+                            <FormBottom>
+                                <p onClick={showHabitForm}>Cancelar</p>
+                                <div>Salvar</div>
+                            </FormBottom>
+                        </form>
+                    </FormSection>
+                )}
                 <HabitSection>
                     {userHabits.length === 0 ? (
                         <span>
@@ -63,18 +103,54 @@ const AddButton = styled.div`
 `;
 
 const HabitSection = styled.section`
-    margin-top: 29px;
-    color: #666666;
+  margin-top: 29px;
+  color: #666666;
 
-    span {
-        font-size: 18px;
-    }
+  span {
+    font-size: 18px;
+  }
 `;
 
 const FormSection = styled.section`
-    margin-top: 20px;
-    height: 180px;
-    padding: 18px 17px 15px 19px;
-    background-color: #FFFFFF;
-    border-radius: 5px;
+  margin-top: 20px;
+  height: 180px;
+  padding: 18px 17px 15px 19px;
+  background-color: #ffffff;
+  border-radius: 5px;
+`;
+
+const DaysContainer = styled.div`
+    margin-top: 8px;
+    display: flex;
+    column-gap: 4px;
+
+    div {
+        width: 30px;
+        height: 30px;
+    }
+`;
+
+const FormBottom = styled.div`
+    margin-top: 29px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    column-gap: 23px;
+    align-items: center;
+    font-size: 16px;
+
+    p{
+        color: #52B6FF;
+    }
+
+    div {
+        width: 84px;
+        height: 35px;
+        background-color: #52B6FF;
+        color: #FFFFFF;
+        border-radius: 4.64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 `;
