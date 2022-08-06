8f312c07b64a740ff340ef6fea43f8ef675968c1
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-06 19:50:40 -0300

    History calendar working

--- src/components/HistoricalData.js
+++ src/components/HistoricalData.js
@@ -1,10 +1,46 @@
+import dayjs from "dayjs";
+import { useContext, useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import styled from "styled-components";
 import { MainAuth } from "../common/MainAuth";
 import { TitleSection } from "../common/TitleSection";
+import LoginContext from "../contexts/LoginContext";
+import { getHistory } from "../services/trackit";
 import Footer from "./Footer";
 import Header from "./Header";
 
 export default function HistoricalData() {
+    const [data, setData] = useState([]);
+    const [days, setDays] = useState([]);
+
+    const { token } = useContext(LoginContext);
+
+    useEffect(() => {
+        getHistory(token)
+            .then((res) => {
+                setData(res.data);
+                setDays(res.data.map(v => v.day));
+            })
+            .catch(() => {
+                alert("Houve um erro ao carregar o histórico");
+            });
+    }, [token]);
+
+    function assignColor({date}) {
+        const tileDate = dayjs(date).format("DD/MM/YYYY");
+        const today = dayjs().format("DD/MM/YYYY");
+        
+        if (tileDate !== today && data.some(v => v.day === tileDate)) {
+            if (data[days.indexOf(tileDate)].habits.some(v => v.done === false)) {
+                return "incomplete";
+            }
+            else {
+                return "complete";
+            }
+        }
+    }
+
     return (
         <>
             <MainAuth>
@@ -12,17 +48,49 @@ export default function HistoricalData() {
                 <TitleSection>
                     <h1>Histórico</h1>
                 </TitleSection>
-                <SpanWrapper>
-                    <span>Em breve você poderá ver o histórico dos seus hábitos aqui!</span>
-                </SpanWrapper>
+                <CalendarContainer>
+                    <Calendar
+                        locale="pt-br"
+                        calendarType="US"
+                        className="calendar"
+                        formatDay={(locale, date) => dayjs(date).format('DD')}
+                        tileClassName={assignColor}
+                    />
+                </CalendarContainer>
                 <Footer />
             </MainAuth>
         </>
     );
 }
 
-const SpanWrapper = styled.div`
-    margin-top: 20px;
-    color: #666666;
-    font-size: 18px;
+const CalendarContainer = styled.div`
+  margin-top: 12px;
+
+  .calendar {
+    border: 0px;
+    border-radius: 10px;
+    width: 100%;
+    height: 402px;
+    max-width: 550px;
+  }
+
+  .calendar button {
+    margin: 3px 0;
+  }
+
+  .react-calendar__tile {
+    padding: 20px 6.6667px;
+  }
+
+  .complete, .incomplete {
+    clip-path: circle(35%);
+  }
+
+  .complete {
+    background-color: #8CC654;
+  }
+
+  .incomplete {
+    background-color: #EA5766;
+  }
 `;
