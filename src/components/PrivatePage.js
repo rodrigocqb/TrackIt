c25cff749379365deadb2e72754f7937d96b99e0
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-26 19:59:38 -0300

    Private page alert translated to English

--- src/components/PrivatePage.js
+++ src/components/PrivatePage.js
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import LoginContext from "../contexts/LoginContext";
 import UserContext from "../contexts/UserContext";
@@ -6,13 +7,16 @@ import UserContext from "../contexts/UserContext";
 export default function PrivatePage({ children }) {
   const { token, setToken } = useContext(LoginContext);
   const { setUser } = useContext(UserContext);
+
   const navigate = useNavigate();
+
+  const { t } = useTranslation();
 
   const localUser = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
     if (!localUser) {
-      alert("Por favor, faça login antes de acessar essa página");
+      alert(t("pleaseLogIn"));
       return navigate("/");
     } else if (!token) {
       setToken(localUser.token);
